cbb7747902437ac7da4ba6bd9bd437621c31e7ee
Björn Larsson <bjornlarsson@bln-macbook.local>
2020-06-12 12:29:08 +0200

    update

--- test-jwt.py
+++ test-jwt.py
@@ -1,1 +1,15 @@
 
+import jwt
+import datetime
+import secret
+
+# generate JWT
+payload = {
+'iss': secret.api_key,
+'exp': datetime.datetime.now() + datetime.timedelta(minutes=5)
+}
+
+jwt_encoded = str(jwt.encode(payload, secret.api_sec))
+jwt_encoded = jwt_encoded[2:-1]
+
+print (jwt_encoded)
