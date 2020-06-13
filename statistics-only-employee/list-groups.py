e2a5c8ae247dab6447c004557804f3fbb381381a
Björn Larsson <bjornlarsson@bln-macbook.local>
2020-06-13 04:46:44 +0200

    update

--- statistics-only-employee/list-groups.py
+++ statistics-only-employee/list-groups.py
@@ -1,6 +1,9 @@
 import http.client
+import sys
 import json
 import jwt
+from datetime import datetime
+from datetime import timedelta
 sys.path.append('../')
 import secret
 
@@ -16,37 +19,10 @@ psw = jwt_encoded[2:-1]
 conn = http.client.HTTPSConnection("api.zoom.us")
 headers = { 'authorization': "Bearer "+psw }
 
-conn.request("GET", "/v2/groups/"+groupid.groupId+"/members?page_number=1&page_size=300", headers=headers)
+conn.request("GET", "/v2/groups", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 raw_data = json.loads(data)
 
-page_number = (raw_data['page_number'])
-page_count = (raw_data['page_count'])
-
-print(page_number)
-print(page_count)
-
-employee=([x["email"] for x in raw_data["members"]])
-
-with open('employee.txt', 'w') as f:
-    for item in employee:
-        f.write("%s\n" % item)
-
-while page_number != page_count:
-
-    print(page_number+1)
-    page_number = page_number+1
-    
-    conn.request("GET", "/v2/groups/"+groupid.groupId+"/members?page_number="+str(page_number)+"&page_size=300", headers=headers)
-    
-    res = conn.getresponse()
-    data = res.read()
-    raw_data = json.loads(data)
-    
-    employee=([x["email"] for x in raw_data["members"]])
-    
-    with open('employee.txt', 'a') as f:
-        for item in employee:
-            f.write("%s\n" % item)
+print (raw_data)
