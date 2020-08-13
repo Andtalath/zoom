d7c98d144b09bbddf290e2d0c61b613cbfd15b5c
Björn Larsson <66303636+bln-kth@users.noreply.github.com>
2020-08-13 15:36:12 +0200

    update

--- delete-users/delete-users.py
+++ delete-users/delete-users.py
@@ -1,1 +1,11 @@
-
+import http.client
+import json
+import sys
+import jwt
+import datetime
+from datetime import timedelta
+import time
+from ldap3 import Server, Connection, ALL
+import ldapinfo
+sys.path.append('../')
+import secret
